ac3ea6b4b72a152216de55eae7234551689e22ae
Newyee1994 <n799230342y@qq.com>
2021-06-20 20:01:58 +0800

    改用 sqlite3 数据库

--- mysite/__init__.py
+++ mysite/__init__.py
@@ -1,5 +1,5 @@
-# 导入第三方模块，用来操作mysql数据库
-import pymysql
-
-# 告诉 Django 用 pymysql 代替默认的 mysqldb 连接 MySQL 数据库
-pymysql.install_as_MySQLdb()
+# # 导入第三方模块，用来操作mysql数据库
+# import pymysql
+#
+# # 告诉 Django 用 pymysql 代替默认的 mysqldb 连接 MySQL 数据库
+# pymysql.install_as_MySQLdb()
