3aed69fb5d41c7e528a7d94020d0e034f43eba12
Newyee1994 <n799230342y@qq.com>
2021-06-20 17:06:15 +0800

    Debug

--- blog/views.py
+++ blog/views.py
@@ -41,7 +41,7 @@ def post_edit(request, pk):
             post.author = request.user
             # post.published_date = timezone.now()
             post.save()
-            return redirect('', pk=post.pk)
+            return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
