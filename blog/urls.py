3cfca6195c4e8d12cb362d057a36d1c3a124f2b3
Newyee1994 <n799230342y@qq.com>
2021-06-20 17:54:01 +0800

    添加评论：展示和发布

--- blog/urls.py
+++ blog/urls.py
@@ -10,4 +10,6 @@ urlpatterns = [
     url(r'^post/(?P<pk>[0-9]+)/remove/$', views.post_remove, name='post_remove'),
     url(r'^post/new/$', views.post_new, name='post_new'),
     url(r'^drafts/$', views.post_draft_list, name='post_draft_list'),
+
+    url(r'^post/(?P<pk>[0-9]+)/comment/$', views.add_comment_to_post, name='add_comment_to_post'),
 ]
